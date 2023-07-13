d2b6c4fb9505570afd31ce26f5f56cd7e23f743e
maxjhernandez <maximojhernandezla@gmail.com>
2023-07-13 18:49:55 -0400

    env.exaple

--- src/dao/dbConfig.js
+++ src/dao/dbConfig.js
@@ -6,7 +6,7 @@ const MONGO_URL = config.mongoUrl;
 try {
     await mongoose.connect(MONGO_URL);
     // await mongoose.connect('mongodb+srv://maximojhernandezla:Iru151220@cluster0mh.tlhs7mz.mongodb.net/newecommerce?retryWrites=true&w=majority')
-    // console.log('***Conectado a la BDD***');
+    console.log('***Conectado a la BDD***');
 } catch (error) {
     console.log(error);
 }
